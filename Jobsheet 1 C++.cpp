d9cf6ba6107aebcff408a023f0db391dbab8f2fa
Andika Rizaldy <64571888+4ndikaRizaldy@users.noreply.github.com>
2020-05-11 16:52:59 +0800

    Create Jobsheet 1 C++.cpp
    Menampilkan array 1 dimensi

---
+++ Jobsheet 1 C++.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+	cout<<"Nama     : Andika Rizaldy\nNim      : F1B019024\nKelompok : 6\n"<<endl;
+	int my_array[5] = {2,3,4,5,6};
+	cout<<"Nim Mahasiswa pertama = "<<my_array[0]<<endl;
+	cout<<"Nim Mahasiswa pertama = "<<my_array[1]<<endl;
+	cout<<"Nim Mahasiswa pertama = "<<my_array[2]<<endl;
+	cout<<"Nim Mahasiswa pertama = "<<my_array[3]<<endl;
+	cout<<"Nim Mahasiswa pertama = "<<my_array[4]<<endl;
+}
