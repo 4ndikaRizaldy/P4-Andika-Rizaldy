7e48f5b9605214dc67fb24620f35d22d617a8b9b
Andika Rizaldy <64571888+4ndikaRizaldy@users.noreply.github.com>
2020-05-11 16:51:18 +0800

    Create Jobsheet 1 C.c
    Menampilkan array 1 dimensi

---
+++ Jobsheet 1 C.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+	printf("Nama     : Andika Rizaldy\nNim      : F1B019024\nKelompok : 6\n");
+	int my_array[5] = {2,3,4,5,6};
+	printf("Nim Mahasiswa pertama = %d",my_array[0]);
+	printf("\nNim Mahasiswa pertama = %d",my_array[1]);
+	printf("\nNim Mahasiswa pertama = %d",my_array[2]);
+	printf("\nNim Mahasiswa pertama = %d",my_array[3]);
+	printf("\nNim Mahasiswa pertama = %d",my_array[4]);
+}
