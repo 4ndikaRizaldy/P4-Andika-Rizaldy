aed84a89196d513b56ebcd896401e0adccc91de3
Andika Rizaldy <64571888+4ndikaRizaldy@users.noreply.github.com>
2020-05-11 17:26:55 +0800

    Create Jobsheet 2 C++.cpp
    mengisi dan menampilkan Elemen Array (Dinamis)

---
+++ Jobsheet 2 C++.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+	cout<<"Nama     : Andika Rizaldy\nNim      : F1B019024\nKelompok : 6\n";
+	int x[5];
+    cout<<"Masukkan Akhiran Nim Mahasiswa 1: ";
+    cin>>x[0];
+    cout<<"Masukkan Akhiran Nim Mahasiswa 2: ";
+    cin>>x[1];
+    cout<<"Masukkan Akhiran Nim Mahasiswa 3: ";
+    cin>>x[2];
+    cout<<"Masukkan Akhiran Nim Mahasiswa 4: ";
+    cin>>x[3];
+    cout<<"Masukkan Akhiran Nim Mahasiswa 5: ";
+    cin>>x[4];
+    cout<<endl;
+    cout<<"Akhiran Nim Mahasiswa pertama = "<<x[0]<<endl; 
+    cout<<"Akhiran Nim Mahasiswa kedua   = "<<x[1]<<endl; 
+    cout<<"Akhiran Nim Mahasiswa ketiga  = "<<x[2]<<endl; 
+    cout<<"Akhiran Nim Mahasiswa keempat = "<<x[3]<<endl; 
+    cout<<"Akhiran Nim Mahasiswa kelima  = "<<x[4]<<endl; 
+}
